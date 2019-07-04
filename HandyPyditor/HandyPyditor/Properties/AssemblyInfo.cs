b615015d583b14426630506034589ea137570f97
NaBian <836904362@qq.com>
2019-07-04 23:57:07 +0800

    add basic files

---
+++ HandyPyditor/HandyPyditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HandyPyditor")]
+[assembly: AssemblyDescription("a simple editor for python")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HandyPyditor")]
+[assembly: AssemblyCopyright("Copyright © HandyOrg 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HandyPyditor/HandyPyditor/Tools/Win32.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HandyPyditor.Tools
+{
+    public class Win32
+    {
+        public struct Point
+        {
+            public int X;
+
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        /// <summary>   
+        /// 获取鼠标的坐标   
+        /// </summary>   
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetCursorPos(out Point point);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool GenerateConsoleCtrlEvent(int dwCtrlEvent, int dwProcessGroupId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool SetConsoleCtrlHandler(IntPtr handlerRoutine, bool add);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool AttachConsole(int dwProcessId);
+    }
+}
